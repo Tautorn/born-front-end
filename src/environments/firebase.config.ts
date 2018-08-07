942eb852d75897550d5bcc4b0cdb17d57dc07df8
Pedro Eduardo de Paula Naves <pedronaves@gmail.com>
2018-08-07 12:31:31 -0300

    First commit

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,19 @@
+export const FirebaseConfig = {
+  /*firebase: {
+    apiKey: 'AIzaSyCsxcQ9REe9OsWbibEcBCwt1fxUnh7JClY',
+    authDomain: 'zupchallenge.firebaseapp.com',
+    databaseURL: 'https://zupchallenge.firebaseio.com',
+    projectId: 'zupchallenge',
+    storageBucket: 'zupchallenge.appspot.com',
+    messagingSenderId: '939768500077'
+  }*/
+
+  firebase: {
+    apiKey: 'AIzaSyAJ-a7KdWp3lb0VBwGaBba0KxVq59L41Ow',
+    authDomain: 'born-front-end.firebaseapp.com',
+    databaseURL: 'https://born-front-end.firebaseio.com',
+    projectId: 'born-front-end',
+    storageBucket: 'born-front-end.appspot.com',
+    messagingSenderId: '664125934442'
+  }
+};
